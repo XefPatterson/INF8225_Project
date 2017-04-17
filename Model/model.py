fixed model for sample softmax. Orientation for attention labels.

Model/model.py
@@ -127,7 +127,11 @@ class Seq2Seq(object):
                     for i in range(self.buckets[bucket_id][1]):
                         self.outputs_test[bucket_id].append(tf.nn.xw_plus_b(
                             self.outputs[bucket_id][i], self.output_projection[0], self.output_projection[1]))
-                        # break
+            else:
+                for bucket_id in range(len(self.buckets)):
+                    for i in range(self.buckets[bucket_id][1]):
+                        self.outputs_test[bucket_id].append(self.outputs[bucket_id][i])
+
             if FLAGS.use_attention:
                 self.attentions = model_infos[2]
 

Model/utils.py
@@ -98,7 +98,7 @@ def plot_attention(questions, attentions, predictions, idx_to_char, idx_to_word,
         # per rows: question attention
         # per cols: answer generation
         data = np.stack(attention, axis=1)
-        tuples[i].imshow(data, vmin=0, vmax=1, cmap='Greys', interpolation="none")
+        tuples[i].imshow(data, cmap='Greys', interpolation="none")
         tuples[i].set_xticks(range(len(answer)))
         if not char_decoder:
             tuples[i].set_xticklabels(answer, fontsize='x-small', rotation='vertical')